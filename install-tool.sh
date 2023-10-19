29a8ea0707f0de627c901454bbb7e13c0c1f3125
solidshadw <samuelsilva10.ss@gmail.com>
2023-10-19 12:49:37 -0600

    update Script

--- install-tool.sh
+++ install-tool.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 echo ""
 echo ""
 echo "                    ' _ ._  _ , _ ._'"
@@ -17,7 +16,7 @@ echo "|______________________________________________________|"
 echo ""
 echo ""
 echo ""
-echo ""
+echo ""b
 
 # Initialize an array to store the names of tools that failed to install
 failed_tools=()
@@ -27,26 +26,125 @@ successful_tools=()
 RED='\033[0;31m'  # Red color
 NC='\033[0m'      # No color
 GREEN='\033[0;32m' # Green color
+BLUE='\033[0;34m'
 
 # Function to install Go
+
 install_go() {
-    # Check if Go is already installed
-    if ! command -v go; then
-        echo "Installing Go..."
+    echo "---------------------------------------------------------------"
+    echo "Installing Go..."
+    if ! command -v /usr/local/go/bin/go; then
         # Download the latest stable Go version from the official website
-        wget https://go.dev/dl/$(curl -s https://go.dev/dl/ | grep -o 'go[0-9]*\.[0-9]*\.[0-9]*\.linux-amd64\.tar\.gz' | head -n 1)
+        wget https://go.dev/dl/$(curl -s https://go.dev/dl/ | grep -o 'go[0-9]*\.[0-9]*\.[0-9]*\.linux-amd64\.tar\.gz' | head -n 1) >/dev/null 2>&1
         # Extract and install Go
         sudo tar -C /usr/local -xzf go*.linux-amd64.tar.gz
-        # Add Go binaries to PATH
-        echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-        source ~/.bashrc
-	export PATH=$PATH:/usr/local/go/bin
+      # Check if the export line is already present in .bashrc
+        if ! grep -q "export PATH=\$PATH:/usr/local/go/bin" ~/.bashrc; then
+            echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+            source ~/.bashrc
+        fi
+        # Check if the export line is already present in .zshrc
+        if ! grep -q "export PATH=\$PATH:/usr/local/go/bin" ~/.zshrc; then
+            echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+            source ~/.zshrc
+        fi
         # Clean up the downloaded tar.gz file
         rm go*.linux-amd64.tar.gz
         echo "Go installed successfully!"
-	go version
+            go version
     else
-        echo "Go is already installed."
+        echo -e "${BLUE}Go is already installed${NC}"
+        go version
+    fi
+}
+
+# # Function to install nuclei url: https://github.com/projectdiscovery/nuclei
+install_nuclei() {
+    echo "---------------------------------------------------------------"
+    echo "Installing nuclei..."
+    
+    # Check if nuclei is installed in ~/go/bin/nuclei
+    if [ -x "$HOME/go/bin/nuclei" ]; then
+        echo -e "${BLUE}nuclei is already installed in $HOME/go/bin${NC}"
+        
+        # If not found in /usr/local/bin, copy it there. So, that you are aware of what go tools you have installed
+        if [ ! -x "/usr/local/bin/nuclei" ]; then
+            sudo cp "$HOME/go/bin/nuclei" "/usr/local/bin/nuclei"
+            echo "nuclei copied to /usr/local/bin"
+        fi
+    fi
+
+    # Check if nuclei is installed in /usr/local/bin/nuclei
+    if [ -x "/usr/local/bin/nuclei" ]; then
+        echo -e "${BLUE}nuclei is already installed in /usr/local/bin${NC}"
+        
+        # If not found in ~/go/bin, copy it there
+        if [ ! -x "$HOME/go/bin/nuclei" ]; then
+            cp "/usr/local/bin/nuclei" "$HOME/go/bin/nuclei"
+            echo "nuclei copied to $HOME/go/bin"
+        fi
+    fi
+
+    # If not installed in either location, install it
+    if [ ! -x "$HOME/go/bin/nuclei" ] && [ ! -x "/usr/local/bin/nuclei" ]; then
+        if ! command -v nuclei; then
+            if error_message=$(go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest 2>&1 >/dev/null); then
+                sudo cp $HOME/go/bin/nuclei /usr/local/bin
+                echo "nuclei installed successfully!"
+                successful_tools+=("nuclei")
+            else
+                echo -e "${RED}Failed to install nuclei.${NC}"
+                failed_tools+=("nuclei: $error_message")
+            fi
+        else
+            echo -e "${BLUE}nuclei is already installed${NC}"
+        fi
+    fi
+}
+
+# # Function to install subfinder url: https://github.com/projectdiscovery/subfinder
+
+install_subfinder() {
+    echo "---------------------------------------------------------------"
+    echo "Installing subfinder..."
+
+    # Check if subfinder is found in ~/go/bin/subfinder
+    if [ -x "$HOME/go/bin/subfinder" ]; then
+        echo -e "${BLUE}subfinder is already installed in $HOME/go/bin${NC}"
+        
+        # If not found in /usr/local/bin, copy it there. So, that you are aware of what go tools you have installed
+        if [ ! -x "/usr/local/bin/subfinder" ]; then
+            sudo cp "$HOME/go/bin/subfinder" "/usr/local/bin/subfinder"
+            echo "subfinder copied to /usr/local/bin"
+        fi
+    fi
+
+    # Check if subfinder is found in /usr/local/bin/subfinder
+    if [ -x "/usr/local/bin/subfinder" ]; then
+        echo -e "${BLUE}subfinder is already installed in /usr/local/bin${NC}"
+        
+        # If not found in ~/go/bin, copy it there
+        if [ ! -x "$HOME/go/bin/subfinder" ]; then
+            cp "/usr/local/bin/subfinder" "$HOME/go/bin/subfinder"
+            echo "subfinder copied to $HOME/go/bin"
+        fi
+    fi
+
+    #If not installed in either location, install it
+    if [ ! -x "$HOME/go/bin/subfinder" ] && [ ! -x "/usr/local/bin/subfinder" ]; then    
+        if ! command -v subfinder; then
+            if error_message=$(go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest 2>&1 >/dev/null); then
+                udo cp $HOME/go/bin/subfinder /usr/local/bin
+                echo "subfinder installed successfully!"
+                successful_tools+=("subfinder")
+            else
+                echo -e "${RED}Failed to install subfinder.${NC}"
+                failed_tools+=("subfinder: $error_message")
+            fi
+        else
+            echo -e "${BLUE}subfinder is already installed${NC}"
+        fi
+        
     fi
 }
 
@@ -54,83 +152,97 @@ install_go() {
 install_naabu() {
     echo "---------------------------------------------------------------"
     echo "Installing naabu..."
-    if ! command -v naabu; then
-        # Download the latest naabu release
-       wget $(curl -s https://api.github.com/repos/projectdiscovery/naabu/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | grep "_linux_amd64.zip")
-       # Extract the downloaded zip file
-       if error_message=$(unzip naabu_*_linux_amd64.zip -d $HOME/go/bin); then
-       # Move naabu binary to /usr/local/bin (you may need sudo)
-           sudo cp $HOME/go/bin/naabu /usr/local/bin/
-           # Clean up
-           rm naabu_*_linux_amd64.zip
-           echo "naabu installed successfully!"
-           successful_tools+=("naabu")
-        else
-           echo -e "${RED}Failed to install naabu.${NC}"
-           failed_tools+=("naabu: $error_message")
-        fi
-   else
-           echo "naabu is already installed."
-   fi
-}
 
-# Function to install nuclei url: https://github.com/projectdiscovery/nuclei
-# check for installation of nuclei in linux
-install_nuclei() {
-    echo "---------------------------------------------------------------"
-    echo "Installing nuclei..."
-    if ! command -v nuclei; then
-        if error_message=$(go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest); then
-            sudo cp $HOME/go/bin/nuclei /usr/local/bin
-            echo "nuclei installed successfully!"
-            successful_tools+=("nuclei")
-        else
-            echo -e "${RED}Failed to install nuclei.${NC}"
-            failed_tools+=("nuclei: $error_message")
+ # Check if naabu is found in ~/go/bin/naabu
+    if [ -x "$HOME/go/bin/naabu" ]; then
+        echo -e "${BLUE}naabu is already installed in $HOME/go/bin${NC}"
+        
+        # If not found in /usr/local/bin, copy it there. So, that you are aware of what go tools you have installed
+        if [ ! -x "/usr/local/bin/naabu" ]; then
+            sudo cp "$HOME/go/bin/naabu" "/usr/local/bin/naabu"
+            echo "naabu copied to /usr/local/bin"
         fi
-    else
-            echo "nuclei is already installed."
+    fi
+
+    # Check if naabu is found in /usr/local/bin/naabu
+    if [ -x "/usr/local/bin/naabu" ]; then
+        echo -e "${BLUE}naabu is already installed in /usr/local/bin${NC}"
+        
+        # If not found in ~/go/bin, copy it there
+        if [ ! -x "$HOME/go/bin/naabu" ]; then
+            cp "/usr/local/bin/naabu" "$HOME/go/bin/naabu"
+            echo "naabu copied to $HOME/go/bin"
+        fi
+    fi
+
+    #If not installed in either location, install it
+    if [ ! -x "$HOME/go/bin/subfinder" ] && [ ! -x "/usr/local/bin/subfinder" ]; then    
+        if ! command -v naabu; then
+            # Download the latest naabu release
+            wget $(curl -s https://api.github.com/repos/projectdiscovery/naabu/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | grep "_linux_amd64.zip") >/dev/null 2>&1
+            # Extract the downloaded zip file
+            if error_message=$(unzip naabu_*_linux_amd64.zip -d $HOME/go/bin 2>&1 >/dev/null); then
+                # Move naabu binary to /usr/local/bin (you may need sudo)
+                sudo cp $HOME/go/bin/naabu /usr/local/bin/
+                # Clean up
+                rm naabu_*_linux_amd64.zip
+                echo "naabu installed successfully!"
+                successful_tools+=("naabu")
+            else
+                echo -e "${RED}Failed to install naabu.${NC}"
+                failed_tools+=("naabu: $error_message")
+            fi
+        else
+           echo -e "${BLUE}naabu is already installed${NC}"
+         fi
     fi
 }
-
-# Function to install subfinder url: https://github.com/projectdiscovery/subfinder
-# check for installation of subfinder in linux
-install_subfinder() {
-    echo "---------------------------------------------------------------"
-    echo "Installing subfinder..."
-    if ! command -v subfinder; then
-        if error_message=$(go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest); then
-            sudo cp $HOME/go/bin/subfinder /usr/local/bin
-            echo "subfinder installed successfully!"
-            successful_tools+=("subfinder")
-        else
-            echo -e "${RED}Failed to install subfinder.${NC}"
-            failed_tools+=("subfinder: $error_message")
-        fi
-    elserm 0
-            echo "subfinder is already installed."
-    fi
-}
-
-# # Function to install gowitness url: https://github.com/sensepost/gowitness
-# install_gowitness() {
-#     echo "---------------------------------------------------------------"
-#     echo "Installing gowitness..."
-#     go install github.com/sensepost/gowitness@latest
-#     sudo cp $HOME/go/bin/gowitness /usr/local/bin
-#     echo "gowitness installed successfully!"
-#     successful_tools+=("gowitness")
-# }
 
 # # Function to install httpx url: https://github.com/projectdiscovery/httpx
-# install_httpx() {
-#     echo "---------------------------------------------------------------"
-#     echo "Installing httpx..."
-#     go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-#     sudo cp $HOME/go/bin/httpx /usr/local/bin
-#     echo "httpx installed successfully!"
-#     successful_tools+=("httpx")
-# }
+
+install_httpx() {
+    echo "---------------------------------------------------------------"
+    echo "Installing httpx..."
+
+    # Check if httpx is found in ~/go/bin/httpx
+    if [ -x "$HOME/go/bin/httpx" ]; then
+        echo -e "${BLUE}httpx is already installed in $HOME/go/bin${NC}"
+        
+        # If not found in /usr/local/bin, copy it there. So, that you are aware of what go tools you have installed
+        if [ ! -x "/usr/local/bin/httpx" ]; then
+            sudo cp "$HOME/go/bin/httpx" "/usr/local/bin/httpx"
+            echo "httpx copied to /usr/local/bin"
+        fi
+    fi
+
+    # Check if httpx is found in /usr/local/bin/httpx
+    if [ -x "/usr/local/bin/httpx" ]; then
+        echo -e "${BLUE}httpx is already installed in /usr/local/bin${NC}"
+        
+        # If not found in ~/go/bin, copy it there
+        if [ ! -x "$HOME/go/bin/httpx" ]; then
+            cp "/usr/local/bin/httpx" "$HOME/go/bin/httpx"
+            echo "httpx copied to $HOME/go/bin"
+        fi
+    fi
+
+    #If not installed in either location, install it
+    if [ ! -x "$HOME/go/bin/httpx" ] && [ ! -x "/usr/local/bin/httpx" ]; then    
+        if ! command -v httpx; then
+            if error_message=$(go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest 2>&1 >/dev/null); then
+                udo cp $HOME/go/bin/httpx /usr/local/bin
+                echo "httpx installed successfully!"
+                successful_tools+=("httpx")
+            else
+                echo -e "${RED}Failed to install httpx.${NC}"
+                failed_tools+=("httpx: $error_message")
+            fi
+        else
+            echo -e "${BLUE}httpx is already installed${NC}"
+        fi
+        
+    fi
+}
 
 # # Function to install notify url: https://github.com/projectdiscovery/notify
 # install_notify() {
@@ -225,20 +337,20 @@ install_subfinder() {
 
 # Main script
 install_go
-install_naabu
 install_nuclei
 install_subfinder
-install_gowitness
-install_httpx
-install_notify
-install_assetfinder
-install_waybackurls
-install_gf
-install_shortscan
-install_anew
-install_simplehttpserver
-install_ffuf
-install_amass
+install_naabu
+# install_gowitness
+# install_httpx
+# install_notify
+# install_assetfinder
+# install_waybackurls
+# install_gf
+# install_shortscan
+# install_anew
+# install_simplehttpserver
+# install_ffuf
+# install_amass
 
 
 echo ""
@@ -248,7 +360,7 @@ echo ""
 if [ ${#successful_tools[@]} -eq 0 ]; then
     echo "No tools were successfully installed."
 else
-    echo "The following tools ${GREEN}successfully${NC} installed:"
+    echo -e "The following tools ${GREEN}successfully${NC} installed:"
     for tool_success in "${successful_tools[@]}"; do
         echo -e "${GREEN}$tool_success${NC}"
     done
